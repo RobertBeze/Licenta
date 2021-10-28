4e02021d7d986befc7932c6a0a0d049f7b0d868e
RobertBeze <59908878+RobertBeze@users.noreply.github.com>
2021-10-28 13:57:47 +0300

    Delete fleetmanager directory

--- fleetmanager/pages/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-def home_view(request):
-	#print(request.user)
-	#print(request.user.is_authenticated)
-	return render(request, "home.html", {})
