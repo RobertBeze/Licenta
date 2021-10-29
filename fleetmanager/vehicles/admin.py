1715523f75eee1fe35d590bbcdd51bf2ab69afc2
RobertBeze <robert.beze00@e-uvt.ro>
2021-10-29 17:07:07 +0300

    Mod 29/10/2021
    Adaugat clasele vehicle si category, modificat baza de date, modificat navbar in functie de tipul de user

--- fleetmanager/vehicles/admin.py
+++ fleetmanager/vehicles/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category, Vehicle
+admin.site.register(Category)
+admin.site.register(Vehicle)
--- fleetmanager/vehicles/models.py
+++ fleetmanager/vehicles/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.conf import settings
+
+
+
+
 
 # Create your models here.
+
+class Category(models.Model):
+	category_name = models.CharField(max_length = 50, unique = True, verbose_name = "Categorie")
+
+	def get_name(self):
+		return self.category_name
+
+	def __str__(self):
+		return self.category_name
+
+class Vehicle(models.Model):
+	vehicle_plate = models.CharField(max_length = 10, unique = True, verbose_name = "Număr înmatriculare")
+	vehicle_odometer = models.PositiveIntegerField(verbose_name = "Kilometraj")
+	vehicle_itp = models.DateField(verbose_name = "ITP expiră la")
+	vehicle_rca = models.DateField(verbose_name = "RCA expiră la")
+	vehicle_last_service = models.PositiveIntegerField(verbose_name = "Ultimul service efectuat KM")
+	vehicle_driver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null = True, verbose_name = "Șoferul")
+	vehicle_category = models.ForeignKey(Category, on_delete=models.SET_NULL, null = True, verbose_name = "Categoria")
+
+	def __str__(self):
+		string = "Număr: "+self.vehicle_plate+" | Categorie: "+self.vehicle_category.get_name()
+		return string
+
