073806fcd2d7b2c6adb799a9569b0003e9111b35
RobertBeze <robert.beze00@e-uvt.ro>
2021-11-13 12:59:41 +0200

    modificat aspect

--- fleetmanager/pages/views.py
+++ fleetmanager/pages/views.py
@@ -6,6 +6,9 @@ from django.http import HttpResponse
 def home_view(request):
 	#print(request.user)
 	#print(request.user.is_authenticated)
+	if not request.user.is_authenticated:
+		return render(request, "pages/default.html")
+
 	if request.user.is_superuser:
 		return render(request, "pages/home_admin.html", {})
 	return render(request, "pages/home.html", {})
